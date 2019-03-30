406b8df11921e3b5d8685ccb57a57b073295fe04
emilietexier <45147990+emilietexier@users.noreply.github.com>
2019-03-31 00:08:08 +0100

    added noun extraction using TextBlob

---
+++ Keyword extraction/keyword_extraction.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 30 17:15:26 2019
+
+@author: emili
+"""
+
+from textblob import TextBlob
+
+blob = TextBlob('''
+Climate Change’s Worst Outcome: Thermohaline Shutdown
+
+One of the difficulties that many climate models face is predicting the future. Environmental scientists collect tons of data, and in some instances can foretell the level of certain gases in the future. Yet, understanding how the world will respond is tricky. Wind patterns, ocean currents, and weather are incredibly dynamic systems that influence each other and are influenced by many external forces. Fully modeling how the climate behaves in response to exponentially increasing carbon levels has the equivalent complexity of predicting where a toothpick will land after being dropped in a tornado.
+
+This uncertainty is why, even climate change scientists, tend to downplay what the possible outcomes of climate change are. There simply doesn’t exist an answer with a low enough uncertainty. It is for these reasons, discussion of the possible Thermohaline shutdown, while being incredibly terrifying, are often eschewed. However, based on observation, history, and safe assumptions scientists in publications have voiced their concerns. So let’s explore this possibility.
+
+To start, the Thermohaline circulation is a term used to describe the ocean current phenomena on Earth. The circulation travels across the Earth’s oceans, moving water between all the hemispheres. The currents in this circulation are facilitated by varying density of salt concentration throughout the oceans and the effects of temperature (Hence the name Thermo-haline). Near the equator, the salt concentration is higher due to evaporated water. When the ocean region that contains higher salinity contacts a region with lower salinity, the higher density region is swallowed and submerged by the lower density region. This submersion creates a current that starts at the equator.
+
+This current has a tremendous impact on the climate of regions it travels through. As warm equatorial water travels northward, the water heats up the overall climate in the central eastern United States and European regions. This is the reason these regions have more temperate climates compared to similar latitudinal located regions that are land locked. Additionally, These ocean currents are responsible for moving sea organisms (and human waste) across the hemispheres.
+
+Over the last two decades, there has been a growing concern from the scientific community that the Thermohaline circulation is experiencing a slow down. As global temperatures pass the one degree Celsius mark, the Arctic sheets and regions of Greenland have begun to melt at an alarming rate. The Arctic, which contains roughly 70% of all fresh water on Earth, dilutes the salt concentration in the ocean as the sheets melt.
+
+The decrease in salt concentration impedes on the aforementioned density gradient influenced current. Not surprisingly, Bryden et al. noted that the net flow of the Thermohaline circulation has slowed by 30% since the 1950’s. A slowdown may explain the decrease in temperatures in certain climates. While overall global temperatures increase, the absence of warm currents in naturally occurring regions will result in sinking temperatures. There is still much uncertainty over the affects of cooling currents. If temperatures drop slightly, they might simply counteract the effects of global warming in regions like Europe. This is not to say the rest of the world will be so lucky. In a more grim scenario, a drastic reduction in Thermohaline currents can cause temperatures to greatly drop in regions. If a slowdown continues, Europe and regions dependent on warm currents for their climate can expect an ice age.
+
+A more concerning outcome of a Thermohaline shutdown is the potential triggering of a anoxic event. Anoxic events have been associated with halting of ocean currents and global warming events in Earth’s prehistoric period. As oceans become more stagnant, the life below becomes momentarily more active. Microbial ocean organisms, such as plankton, are given the opportunity to reproduced in large numbers. This is the same idea why drinking running water is safer than still water. Running water is less likely to foster bacterial growth.
+
+As the biomass of the ocean explodes, the oxygen content of the ocean begins to decline. Ocean life needs oxygen to survive, yet with too much organic life, obtaining oxygen can become difficult. Regions that have low oxygen content can develop into dead zones, areas in which much of marine life cannot survive in. Kump et al. demonstrated that during such Anoxic events in the past, large quantities of Hydrogen Sulfide gas were released from the Oceans. This noxious gas was most likely related to the large die-off of organic sea life.
+
+This would explain why previous mass extinctions were associated with Thermohaline shutdowns. Much mammalian and plant life cannot survive with hydrogen sulfide gas present in the atmosphere. The same researchers also demonstrated that the release of this gas would have damaged the Ozone layer. Their theory was further supported by fossil records that showed ultraviolet (UV) radiation related scarring. Massive amounts of UV radiation would further facilitate the extinction of land organisms. Human life, as we know it, in these environmental conditions will be impossible.
+
+While the direct causation and links between many of these events are ambiguous, one specific trend is consistent. In all cases in which mass extinctions and Thermohaline shutdowns occurred, the Earth was experiencing record global temperatures and carbon levels. During the Permian-Triassic extinction, atmospheric carbon levels had reached 1000 ppm (Ward, page 71). Current concentrations are at 410.92 ppm. The Earth is still far from reaching cataclysmic carbon levels, yet this is no reason for apathy.
+
+It should be understood that once the Thermohaline circulation is shutdown it cannot be reasonably reversed. Earth has recovered from such events in past, yes. Full recoveries often take a little less than a million years.
+''')
+
+blob2 = TextBlob('''
+        History tells us what may happen next with Brexit & Trump
+
+It seems we’re entering another of those stupid seasons humans impose on themselves at fairly regular intervals. I am sketching out here opinions based on information, they may prove right, or may prove wrong, and they’re intended just to challenge and be part of a wider dialogue.
+
+My background is archaeology, so also history and anthropology. It leads me to look at big historical patterns. My theory is that most peoples’ perspective of history is limited to the experience communicated by their parents and grandparents, so 50–100 years. To go beyond that you have to read, study, and learn to untangle the propaganda that is inevitable in all telling of history. In a nutshell, at university I would fail a paper if I didn’t compare at least two, if not three opposing views on a topic. Taking one telling of events as gospel doesn’t wash in the comparative analytical method of research that forms the core of British academia. (I can’t speak for other systems, but they’re definitely not all alike in this way).
+
+So zooming out, we humans have a habit of going into phases of mass destruction, generally self imposed to some extent or another. This handy list shows all the wars over time. Wars are actually the norm for humans, but every now and then something big comes along. I am interested in the Black Death, which devastated Europe. The opening of Boccaccio’s Decameron describes Florence in the grips of the Plague. It is as beyond imagination as the Somme, Hiroshima, or the Holocaust. I mean, you quite literally can’t put yourself there and imagine what it was like. For those in the midst of the Plague it must have felt like the end of the world.
+
+But a defining feature of humans is their resilience. To us now it seems obvious that we survived the Plague, but to people at the time it must have seemed incredible that their society continued afterwards. Indeed, many takes on the effects of the Black Death are that it had a positive impact in the long term. Well summed up here: “By targeting frail people of all ages, and killing them by the hundreds of thousands within an extremely short period of time, the Black Death might have represented a strong force of natural selection and removed the weakest individuals on a very broad scale within Europe,“ …In addition, the Black Death significantly changed the social structure of some European regions. Tragic depopulation created the shortage of working people. This shortage caused wages to rise. Products prices fell too. Consequently, standards of living increased. For instance, people started to consume more food of higher quality.”
+
+But for the people living through it, as with the World Wars, Soviet Famines, Holocaust, it must have felt inconceivable that humans could rise up from it. The collapse of the Roman Empire, Black Death, Spanish Inquisition, Thirty Years War, War of the Roses, English Civil War… it’s a long list. Events of massive destruction from which humanity recovered and move on, often in better shape.
+
+At a local level in time people think things are fine, then things rapidly spiral out of control until they become unstoppable, and we wreak massive destruction on ourselves. For the people living in the midst of this it is hard to see happening and hard to understand. To historians later it all makes sense and we see clearly how one thing led to another. During the Centenary of the Battle of the Somme I was struck that it was a direct outcome of the assassination of an Austrian Arch Duke in Bosnia. I very much doubt anyone at the time thought the killing of a European royal would lead to the death of 17 million people.
+
+My point is that this is a cycle. It happens again and again, but as most people only have a 50–100 year historical perspective they don’t see that it’s happening again. As the events that led to the First World War unfolded, there were a few brilliant minds who started to warn that something big was wrong, that the web of treaties across Europe could lead to a war, but they were dismissed as hysterical, mad, or fools, as is always the way, and as people who worry about Putin, Brexit, and Trump are dismissed now.            
+''')
+
+print('KEYWORD FOR TEXT 1:')
+
+
+for word2 in blob2.noun_phrases:
+    string2 = word2
+    temp2 = 1
+    for np2 in word2:
+        if (not np2.isalpha() and np2 != '-' and np2 != ' '):
+            temp2 = 0
+    if len(string2) > 5:
+        if temp2 == 1:
+            print(word2)
+
+print(' ')
+
+print ('KEYWORDS FOR TEXT 2:')
+
+for word in blob.noun_phrases:
+    string = word
+    temp = 1
+    for np in word:
+        if (not np.isalpha() and np != '-' and np != ' '):
+            temp = 0
+    if len(string) > 5:
+        if temp == 1:
+            print(word)
+
+
+
