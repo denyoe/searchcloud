5957e5a0de8dc1d09f56c7a531233a7c1df0b0cb
DESKTOP-CIJ17O9\julia <julialudac@yahoo.fr>
2019-03-30 14:51:23 +0100

    Prepared web app functions stubs to get inputs and send outputs

--- server/cloudsearchbe/urls.py
+++ server/cloudsearchbe/urls.py
@@ -6,7 +6,9 @@ app_name = 'cloudsearchbe'
 
 urlpatterns = [
 
+    #url(r'^cloudsearchbe$', views.home, name='home'),
     url(r'^$', views.home, name='home'),
-
-
+    url(r'^find_keywords', views.find_keywords, name='find_keywords'),
+    url(r'^search_fetch', views.find_keywords, name='search_fetch'),
+    url(r'^get_engines', views.find_keywords, name='get_engines'),
 ]
--- server/cloudsearchbe/views.py
+++ server/cloudsearchbe/views.py
@@ -4,8 +4,33 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
+
+# Use processing functions from tools. as t TODO
+
+
 # Create your views here.
 def home(request):
     print("Welcome page")
     context = {}  # Nothing to send
     return render(request, 'cloudsearchbe/first.html', context)
+
+    
+def find_keywords(request):
+    text = request.POST.get('text')
+    kw = t.find_keywords(text) # TODO update? function to call
+    context = {"keywords": kw}
+    return HttpResponse(json.dumps(context), content_type="application/json")
+
+
+def search_fetch(request):
+    text = request.POST.get('keywords')
+    context = t.search_fetch(text) # TODO update? function to call
+    return HttpResponse(json.dumps(context), content_type="application/json")
+
+
+def get_engines(request):
+    engines = t.get_engines() # TODO update? function to call
+    context = {'engines': engines}
+    return HttpResponse(json.dumps(context), content_type="application/json")
+
+
